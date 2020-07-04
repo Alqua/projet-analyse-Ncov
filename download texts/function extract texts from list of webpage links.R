4184c44baba7d97c3caadecbd9590c35acb4e6a5
Alqua <bilatli@hotmail.com>
2020-07-04 16:26:42 +0200

    change names and add TryCatch to function

--- download texts/function extract texts from list of webpage links.R
+++ download texts/function extract texts from list of webpage links.R
@@ -15,12 +15,14 @@ telechargive <- function(dataframe, col1, col2){
     ## following a list in col2
     ##example of call:
     ##telechargive(dataframe = linkA, col1 = "archive", col2 = "id")
+    ##does NOT work with data.table object because of htm2txt
     
-    for( i in rownames(dataframe) ){
+    for( i in rownames(dataframe) ){tryCatch({
         A <- dataframe[i, col1]
         x <- gettxt(as.character(A))
-        y <- as.character(linkA[i, col2])
+        y <- as.character(dataframe[i, col2])
         write.csv(x, file = paste(y, "csv", sep = "."), fileEncoding = "UTF-8" )
         
+    }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
     }
-}
+    }
